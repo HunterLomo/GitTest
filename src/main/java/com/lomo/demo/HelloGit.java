18ef9594fea9834634ff9928704ed0d279021e6a
HunterLomo <majiedota@163.com>
2015-11-03 10:00:46 +0800

    中文：IDEA第一次提交HelloGit.java

---
+++ src/main/java/com/lomo/demo/HelloGit.java
@@ -0,0 +1,11 @@
+package com.lomo.demo;
+
+/**
+ * Created by Administrator on 15-11-3.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Java Hello Git!");
+        System.out.println("Add file to commit by IDEA!");
+    }
+}
